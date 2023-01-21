e989ab6dc212ece4c409c17b908ca7edafc4f5b0
Sourav <johar.sourav97@gmail.com>
2023-01-21 00:24:14 -0800

    Update netsort.go

--- src/netsort.go
+++ src/netsort.go
@@ -1,234 +1,254 @@
 package main
 
 import (
-	"fmt"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
-	"io"
-	"log"
-	"os"
-	"strconv"
-	"net"
-	"time"
-	"sort"
-	"math/big"
-	"math"
+    "fmt"
+    "gopkg.in/yaml.v2"
+    "io/ioutil"
+    "io"
+    "log"
+    "os"
+    "strconv"
+    "net"
+    "time"
+    "sort"
+    "math/big"
+    "math"
 )
 
+
+const SERVER_TYPE = "tcp"
+
+
 type ServerConfigs struct {
-	Servers []struct {
-		ServerId int    `yaml:"serverId"`
-		Host     string `yaml:"host"`
-		Port     string `yaml:"port"`
-	} `yaml:"servers"`
+    Servers []struct {
+        ServerId int    `yaml:"serverId"`
+        Host     string `yaml:"host"`
+        Port     string `yaml:"port"`
+    } `yaml:"servers"`
 }
 
 type Server struct {
-	ServerId int  
-	Host     string
-	Port     string
+    ServerId int  
+    Host     string
+    Port     string
 }
+
 
 func readServerConfigs(configPath string) map[int]Server {
-	f, err := ioutil.ReadFile(configPath)
+    f, err := ioutil.ReadFile(configPath)
 
-	if err != nil {
-		log.Fatalf("could not read config file %s : %v", configPath, err)
-	}
+    if err != nil {
+        log.Fatalf("could not read config file %s : %v", configPath, err)
+    }
 
-	scs := ServerConfigs{}
-	err = yaml.Unmarshal(f, &scs)
+    scs := ServerConfigs{}
+    err = yaml.Unmarshal(f, &scs)
 
-	serverMap := make(map[int]Server)
+    serverMap := make(map[int]Server)
 
-	for _, val := range scs.Servers {
-		serverMap[val.ServerId] = Server{ServerId: val.ServerId, Host: val.Host, Port: val.Port}
-	}
+    for _, val := range scs.Servers {
+        serverMap[val.ServerId] = Server{ServerId: val.ServerId, Host: val.Host, Port: val.Port}
+    }
 
-	return serverMap
+    return serverMap
 }
 
-func readInputFile(inputFilePath string) [][]byte{
-	inputFp, _ := os.Open(inputFilePath)
 
-	// read the file and store byte arrays 
-	byteArray := make([][]byte, 0)
 
-	for {
-		keyValue := make([]byte, 100)
+func handler(conn net.Conn, recvd chan []byte) {
 
-		_, err := io.ReadFull(inputFp, keyValue)
-
-		if err == io.EOF {
-			break
-		}
-		byteArray = append(byteArray, keyValue)
-	}
-
-	inputFp.Close()
-
-	return byteArray
-}
-
-func handler(conn net.Conn, buffer chan []byte) {
-
-	for {
-		tmp := make([]byte, 101)  
+    for {
+        tmp := make([]byte, 101)  
         n, err := conn.Read(tmp)
         if err != nil {
             if err != io.EOF {
                 fmt.Println("read error:", err)
             }
             break
-    	}
+        }
 
-    	buffer <- tmp[:n]
+        recvd <- tmp[:n]
 
-	}
+    }
 }
 
-func reciever(myServerId int, buffer chan []byte, serverMap map[int]Server) {
 
-	l, _ := net.Listen("tcp", serverMap[myServerId].Host +":"+ serverMap[myServerId].Port)
+func listener(myServerId int, recvd chan []byte, serverMap map[int]Server) {
 
-	for {
+    l, _ := net.Listen(SERVER_TYPE,  serverMap[myServerId].Host +":"+ serverMap[myServerId].Port)
 
-		conn, err := l.Accept()
-	        if err != nil {
-	            fmt.Println("Error connecting:", err.Error())
-	            return
-	        }
+    for {
 
-	        go handler(conn, buffer)
-	}
+        conn, err := l.Accept()
+            if err != nil {
+                fmt.Println("Error connecting:", err.Error())
+                return
+            }
+
+            go handler(conn, recvd)
+    }
 }
         
-func sender(array [][]byte, myServerId int, allSent *bool, serverMap map[int]Server, buffer chan []byte) {
 
-	allConnections := make(map[int]net.Conn)
+func sender(array [][]byte, myServerId int, allSent *bool, serverMap map[int]Server, recvd chan []byte) {
 
-	for id, val := range serverMap {
+    allConnections := make(map[int]net.Conn)
 
-		if id == myServerId {
-			continue
-		}
-		connection, err := net.Dial("tcp", val.Host +":"+ val.Port)
-		if err != nil {
+    for id, val := range serverMap {
+
+        if id == myServerId {
+            continue
+        }
+        connection, err := net.Dial(SERVER_TYPE, val.Host +":"+ val.Port)
+        if err != nil {
                 panic(err)
         }
 
         allConnections[id] = connection
         defer connection.Close()
-	} 
+    } 
 
-	sigBits := int(math.Log2(float64(len(serverMap))))
+    sigBits := int(math.Log2(float64(len(serverMap))))
 
-	for _, val := range array {
+    for _, val := range array {
 
-		belongsToServer := int(val[0] >> (8 - sigBits))
+        belongsToServer := int(val[0] >> (8 - sigBits))
 
-		// pre-pend a 0 to signify that we are still streaming
-		stream := []byte{0}
-		val = append(stream, val...)
+        // pre-pend a 0 to signify that we are still streaming
+        stream := []byte{0}
+        val = append(stream, val...)
 
-		if belongsToServer != myServerId {
+        if belongsToServer != myServerId {
 
-			allConnections[belongsToServer].Write(val)
+            allConnections[belongsToServer].Write(val)
 
-		} else {
+        } else {
 
-			buffer <- val
-		}
-	}
+            recvd <- val
+        }
+    }
 
-	// prepare stream complete signal
-	signal := []byte{1, byte(myServerId)}
-	for i := 0; i <= 98; i++ {
-		signal = append(signal, 1)
-	}
 
-	// send stream complete signal to all other nodes
-	for id, _ := range serverMap {
+    // prepare stream complete signal
+    signal := []byte{1, byte(myServerId)}
+    for i := 0; i <= 98; i++ {
+        signal = append(signal, 1)
+    }
 
-		if id == myServerId {
-			continue
-		}
+    // send stream complete signal to all other nodes
+    for id, _ := range serverMap {
 
-		allConnections[id].Write(signal)
-	}
+        if id == myServerId {
+            continue
+        }
 
-	*allSent = true
+        allConnections[id].Write(signal)
+    }
+
+    // kept <- signal 
+    *allSent = true
+
 }
 
+
+func readInputFile(inputFilePath string) [][]byte{
+    inputFp, _ := os.Open(inputFilePath)
+
+    // read the file and store byte arrays 
+    byteArray := make([][]byte, 0)
+
+    for {
+        keyValue := make([]byte, 100)
+
+        _, err := io.ReadFull(inputFp, keyValue)
+
+        if err == io.EOF {
+            break
+        }
+        byteArray = append(byteArray, keyValue)
+    }
+
+    inputFp.Close()
+
+    return byteArray
+}
+
+
 func main() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
+    log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	if len(os.Args) != 5 {
-		log.Fatal("Usage : ./netsort {serverId} {inputFilePath} {outputFilePath} {configFilePath}")
-	}
+    if len(os.Args) != 5 {
+        log.Fatal("Usage : ./netsort {serverId} {inputFilePath} {outputFilePath} {configFilePath}")
+    }
 
-	// What is my serverId
-	serverId, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		log.Fatalf("Invalid serverId, must be an int %v", err)
-	}
+    // What is my serverId
+    serverId, err := strconv.Atoi(os.Args[1])
+    if err != nil {
+        log.Fatalf("Invalid serverId, must be an int %v", err)
+    }
 
-	// Read server configs from file
-	scs := readServerConfigs(os.Args[4])
+    // Read server configs from file
+    scs := readServerConfigs(os.Args[4])
 
-	// read input binary file
-	byteArray := readInputFile(os.Args[2])
+    // read input binary file
+    byteArray := readInputFile(os.Args[2])
 
-	buffer := make(chan []byte)
-	var counter int
+    recvd := make(chan []byte)
+    var counter int
 
-	// start listening to other nodes and accept records and store data into buffer
-	go reciever(serverId, buffer, scs)
+    // start listening to other nodes and accept records and store data into recvd
+    go listener(serverId, recvd, scs)
 
-	time.Sleep(2 * time.Second)
 
-	// start sending data that does not belong to my server
-	allSent := false
-	go sender(byteArray, serverId, &allSent, scs, buffer)
+    time.Sleep(2 * time.Second)
 
-	var toSort [][]byte
-	for {
 
-		var val []byte 
+    allSent := false
+    var toSort [][]byte
 
-		val  = <- buffer
+    // start sending data that does not belong to my server
+    // kept := make(chan []byte, 10000)
+    go sender(byteArray, serverId, &allSent, scs, recvd)
 
-		if val[0] == byte(1) {
-    		counter += 1
-    	} else {
-			toSort = append(toSort, val[1:])
-    	}
 
-    	// break when all nodes have sent their data and current node has sent its data
-    	if counter == len(scs) - 1 && allSent {
-    		break
-    	}
-	}
+    for {
 
-	// perform the sorting
-	sort.Slice(toSort, func(i, j int) bool {
+        var val []byte 
 
-		x := new(big.Int)
-		x.SetBytes(toSort[i][:10])
+        val  = <- recvd
 
-		y := new(big.Int)
-		y.SetBytes(toSort[j][:10])
+        if val[0] == byte(1) {
+            counter += 1
+        } else {
+            toSort = append(toSort, val[1:])
 
-		return x.Cmp(y) != 1
-	}) 
+        }
 
-	// write sorted output
-	outputFilePath := os.Args[3]
-	outputFp, _ := os.Create(outputFilePath)
-	
+        // break when all nodes have sent their data
+        if counter == len(scs) - 1 && allSent {
+            break
+        }
+    }
 
-	for _, record := range toSort {
-		outputFp.Write(record)
-	}
-	outputFp.Close()
+    // perform the sorting
+    sort.Slice(toSort, func(i, j int) bool {
+
+        x := new(big.Int)
+        x.SetBytes(toSort[i][:10])
+
+        y := new(big.Int)
+        y.SetBytes(toSort[j][:10])
+
+        return x.Cmp(y) != 1
+    }) 
+
+    // write sorted output
+    outputFilePath := os.Args[3]
+    outputFp, _ := os.Create(outputFilePath)
+    
+
+    for _, record := range toSort {
+        outputFp.Write(record)
+    }
+    outputFp.Close()
 }
